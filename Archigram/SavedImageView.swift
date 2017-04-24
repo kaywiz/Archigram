5fab7426d10d1ff5030d62041b5e1ec61c215e24
Kelly Wisneski <k.wisneski@wustl.edu>
2017-04-23 21:08:56 -0500

    Userdefaults updates

---
+++ Archigram/SavedImageView.swift
@@ -0,0 +1,16 @@
+//
+//  SavedImageView.swift
+//  Archigram
+//
+//  Created by Kelly Wisneski on 4/23/17.
+//  Copyright © 2017 Jack Crawford. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class SavedImageView: UICollectionViewCell {
+    
+    
+    @IBOutlet weak var imageView: UIImageView!
+}
